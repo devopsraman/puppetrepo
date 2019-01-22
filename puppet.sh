28e44fce588d399be43015568db3f4bcf569434e
Ram Thalla <devopsraman@gmail.com>
2019-01-22 13:57:45 +0000

    Update puppet.sh

--- puppet.sh
+++ puppet.sh
@@ -1,4 +1,7 @@
      rpm -Uvh https://yum.puppetlabs.com/puppet5/puppet5-release-el-7.noarch.rpm
-     yum install -y puppetserver puppet-agent wget git curl
+     yum install -y puppetserver puppet-agent wget  curl
+     wget https://raw.githubusercontent.com/devopsraman/scripts/master/redhat.sh
+     chmod +x redhat.sh
+     ./redhat.sh
      wget https://raw.githubusercontent.com/devopsraman/puppet/master/file.pp
      puppet apply file.pp
